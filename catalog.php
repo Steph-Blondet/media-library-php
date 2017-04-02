673fdcf01015faf86327ab058cccdc58898fb444
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-04-02 08:23:15 -0400

    creating the catalog array

--- catalog.php
+++ catalog.php
@@ -1,4 +1,11 @@
 <?php
+$catalog = array(
+  "Design Patterns",
+  "Forrest Gump",
+  "Beethoven"
+);
+
+
 $pageTitle = "Full Catalog";
 $section = null;
 
@@ -18,8 +25,15 @@ if (isset($_GET["cat"])) {
 
 include("includes/header.php"); ?>
 
-<div class="section page">
-  <h1><?php echo $pageTitle; ?></h1>
+<div class="section catalog page">
+  <div class="wrapper">
+    <h1><?php echo $pageTitle; ?></h1>
+    <ul>
+      <?php foreach ($catalog as $item) {
+        echo "<li>" . $item . "</li>";
+      } ?>
+    </ul>
+  </div>
 </div>
 
 <?php include("includes/footer.php"); ?>
