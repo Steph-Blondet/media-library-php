ffcacec9d57b292f4174378daf4a73c905d74e50
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-04-05 19:08:41 -0400

    creating input fields

--- suggest.php
+++ suggest.php
@@ -10,8 +10,21 @@ include("includes/header.php"); ?>
     <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
 
     <form method="post">
-      <label for="name">Name</label>
-      <input type="text" id="name" name="name"/>
+      <table>
+        <tr>
+          <th><label for="name">Name</label></th>
+          <td><input type="text" id="name" name="name"/></td>
+        </tr>
+        <tr>
+          <th><label for="email">Email</label></th>
+          <td><input type="text" id="email" name="email"/></td>
+        </tr>
+        <tr>
+          <th><label for="details">Suggest Item Details</label></th>
+          <td><textarea name="details" id="details"></textarea></td>
+        </tr>
+      </table>
+      <input type="submit"value="Send"/>
     </form>
 
   </div>
